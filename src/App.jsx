bec8baa6bb457e6823c9ceafa0974fdef82fca00
Thomas Claessens <thomasclssns01@gmail.com>
2019-04-05 00:17:48 +0200

    chore: setup stack, webpack WIP

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+import { hot } from 'react-hot-loader';
+
+const App = () => (
+  <h1>
+    Hello, world.
+    <br />
+  </h1>
+);
+
+export default hot(module)(App);
---
+++ webpack.config.js
@@ -0,0 +1,61 @@
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ['babel-loader', 'eslint-loader'],
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader',
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      inject: true,
+      template: './public/index.html',
+    }),
+  ],
+  devServer: {
+    // Enable gzip compression of generated files.
+    compress: true,
+    // Silence WebpackDevServer's own logs since they're generally not useful.
+    // It will still show compile warnings and errors with this setting.
+    clientLogLevel: 'none',
+    // Enable hot reloading server. It will provide /sockjs-node/ endpoint
+    // for the WebpackDevServer client so it can learn when the files were
+    // updated. The WebpackDevServer client is included as an entry point
+    // in the Webpack development configuration. Note that only changes
+    // to CSS are currently hot reloaded. JS changes will refresh the browser.
+    hot: true,
+    // WebpackDevServer is noisy by default so we emit custom message instead
+    // by listening to the compiler events with `compiler.hooks[...].tap` calls above.
+    quiet: true,
+    // Enable/disable HTTPS,
+    https: false,
+    // Redirect to index.html for file not found
+    historyApiFallback: true,
+    // WebPackDev server serves physical files from
+    contentBase: './public',
+    // It is important to tell WebpackDevServer to use the same "root" path
+    // as we specified in the config. In development, we always serve from /.
+    publicPath: '/',
+    // By default files from `contentBase` will not trigger a page reload.
+    watchContentBase: true,
+  },
+};
