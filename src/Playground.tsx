d785f92713658212a4641670258e62837d5df3e6
Thomas Claessens <thomasclssns01@gmail.com>
2019-04-09 09:28:31 +0200

    feat: fix state switching correctly

--- src/Playground.tsx
+++ src/Playground.tsx
@@ -1,13 +1,55 @@
-import React, { ReactElement, useState } from 'react';
-import Card from './components/Card/Card';
+import React, { ReactElement, useState, Component } from 'react';
+import CardComponent from './components/Card/Card';
 import GameManager from './gameManager/GameManager';
 import Hand from './Hand/Hand';
+import Card from './cards/Card';
 
 interface IProps {
   manager: GameManager;
 }
 
-const Playground: React.SFC<IProps> = ({ manager }): ReactElement<any> | null => {
+interface IState {
+  cardsInHand: Card[];
+}
+
+class Playground extends Component<IProps, IState> {
+  constructor(props) {
+    super(props);
+    this.state = {
+      cardsInHand: [],
+    };
+  }
+  play = async card => {
+    const { manager } = this.props;
+    await manager.playCardFromHand(card);
+  };
+
+  draw = () => {
+    const { manager } = this.props;
+    return manager.drawToHand().then(hand => this.setState({ cardsInHand: hand.cards }));
+  };
+  render() {
+    return (
+      <div>
+        <h1>Playground</h1>
+        <button type="button" onClick={this.draw}>
+          Draw
+        </button>
+        {this.state.cardsInHand.map(card => (
+          <CardComponent
+            name={card.name}
+            cost={card.cost}
+            recruitValue={card.recruit}
+            fightValue={card.fight}
+            onPlay={() => this.play(card)}
+          />
+        ))}
+      </div>
+    );
+  }
+}
+
+/* const Playground: React.SFC<IProps> = ({ manager }): ReactElement<any> | null => {
   const play = async card => {
     await manager.playCardFromHand(card);
   };
@@ -33,6 +75,6 @@ const Playground: React.SFC<IProps> = ({ manager }): ReactElement<any> | null =>
       ))}
     </div>
   );
-};
+}; */
 
 export default Playground;
