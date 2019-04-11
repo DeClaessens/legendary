e7783b9b2915e20aeb79a0c8f5235dcd79400a41
Thomas Claessens <thomasclssns01@gmail.com>
2019-04-11 22:47:35 +0200

    feat: add redux + fix eslint typescript

--- src/index.tsx
+++ src/index.tsx
@@ -3,5 +3,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import todoApp from './reducers/reducer';
 
-ReactDOM.render(<App />, document.getElementById('app'));
+const store = createStore(todoApp);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('app'),
+);
